cd7bca2ed43d4810b55c2940bfcf477af207db77
Calherbe Valcin <mgenerator01@hotmail.com>
2025-03-02 15:48:48 -0500

    Add Comment

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -15,10 +15,27 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    //where are dispatch and getState getting destructured from
+    async (payload,{dispatch, getState})=>{
+        setTimeout(()=>{
+        const {comments} = getState();
+        payload.date = new Date().toISOString();
+        payload.id = comments.commentsArray.length;
+        dispatch(addComment(payload));
+        },2000);
+    }
+)
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action)=>{
+            state.commentsArray.push(action.payload);
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -37,5 +54,5 @@ const commentsSlice = createSlice({
             });
     }
 });
-
+export const {addComment} = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
