bd743b19c52499ed74133f62963b0313476ea316
Calherbe Valcin <mgenerator01@hotmail.com>
2025-03-03 13:49:01 -0500

    Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -17,6 +17,7 @@ import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
 import ReservationScreen from "./ReservationScreen";
+import FavoritesScreen from "./FavoritesScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -144,6 +145,28 @@ const ReservationNavigator = () => {
     </Stack.Navigator>
   );
 };
+const FavoritesNavigator = ()=>{
+  const Stack = createStackNavigator();
+  return(
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen 
+        name='Favorites'
+        component={FavoritesScreen}
+        options={({navigation})=>({
+          title: 'Favorite Campsites',
+          headerLeft: ()=>(
+            <Icon 
+              name='heart'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={navigation.toggleDrawer()}
+            />
+          )
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
 
 const CustomDrawerContent = (props)=>(
   <DrawerContentScrollView {...props}>
@@ -263,6 +286,22 @@ const Main = () => {
             drawerIcon: ({ color }) => (
               <Icon
                 name="address-card"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoritesNavigator}
+          options={{
+            title: "My favorites",
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="heart"
                 type="font-awesome"
                 size={24}
                 iconStyle={{ width: 24 }}
