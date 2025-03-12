b53a171e572d8c89fbc71465f152aa758287c085
Calherbe Valcin <mgenerator01@hotmail.com>
2025-03-12 17:27:20 -0400

    Register Form

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,7 +4,7 @@ import { CheckBox, Button, Input, Icon } from "react-native-elements";
 import * as SecureStore from "expo-secure-store";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const LoginTab = ({navigation}) => {
+const LoginTab = ({ navigation }) => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
@@ -20,7 +20,7 @@ const LoginTab = ({navigation}) => {
           username,
           password,
         })
-      ).catch((error) => console.log("Could not save user info", error))
+      ).catch((error) => console.log("Could not save user info", error));
     } else {
       SecureStore.deleteItemAsync("userInfo").catch((error) =>
         console.log("Could not delete user info", error)
@@ -30,7 +30,7 @@ const LoginTab = ({navigation}) => {
   useEffect(() => {
     SecureStore.getItemAsync("userInfo").then((userData) => {
       const userInfo = JSON.parse(userData);
-      if(userInfo){
+      if (userInfo) {
         setUserName(userInfo.username);
         setPassword(userInfo.password);
         setRemember(true);
@@ -63,112 +63,206 @@ const LoginTab = ({navigation}) => {
         containerStyle={styles.formCheckBox}
       />
       <View style={styles.formButton}>
-        <Button 
-        onPress={() => handleLogin()} 
-        title="Login" 
-        color="#5637DD"
-        icon={
-          <Icon 
-            name='sign-in'
-            type='font-awesome'
-            color='#fff'
-            iconStyle={{marginRight: 10}}
-          />
-        }
-        buttonStyle={{backgroundColor: '#5637DD'}}
-         />
+        <Button
+          onPress={() => handleLogin()}
+          title="Login"
+          color="#5637DD"
+          icon={
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              color="#fff"
+              iconStyle={{ marginRight: 10 }}
+            />
+          }
+          buttonStyle={{ backgroundColor: "#5637DD" }}
+        />
       </View>
       <View style={styles.formButton}>
-        <Button 
-        onPress={() => navigation.navigate('Register')} 
-        title="Register" 
-        type='clear'
-        icon={
-          <Icon 
-            name='user-plus'
-            type='font-awesome'
-            color='blue'
-            iconStyle={{marginRight: 10}}
-          />
-        }
-        titleStyle={{color: 'blue'}}
-         />
+        <Button
+          onPress={() => navigation.navigate("Register")}
+          title="Register"
+          type="clear"
+          icon={
+            <Icon
+              name="user-plus"
+              type="font-awesome"
+              color="blue"
+              iconStyle={{ marginRight: 10 }}
+            />
+          }
+          titleStyle={{ color: "blue" }}
+        />
       </View>
     </View>
   );
 };
-const RegisterTab = ()=>{
-  return <ScrollView></ScrollView>;
+const RegisterTab = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [remember, setRemember] = useState(false);
+
+  const handleRegister = () => {
+    const userInfo = {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+      remember,
+    };
+    console.log(JSON.stringify(userInfo));
+    if (remember) {
+      SecureStore.setItemAsync(
+        "userInfo",
+        JSON.stringify({
+          username,
+          password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      SecureStore.deleteItemAsync("userInfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Input
+          placeholder="Username"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "key" }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="First Name"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Last Name"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope-o" }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <CheckBox
+          title="Remember Me"
+          center
+          checked={remember}
+          onPress={() => setRemember(!remember)}
+          containerStyle={styles.formCheckBox}
+        />
+        <View style={styles.formButton}>
+          <Button
+            onPress={() => handleRegister()}
+            title="Register"
+            color="#5637DD"
+            icon={
+              <Icon
+                name="user-plus"
+                type="font-awesome"
+                color="#fff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{ backgroundColor: "#5637DD" }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 const Tab = createBottomTabNavigator();
 
-const LoginScreen = ()=>{
+const LoginScreen = () => {
   const tabBarOptions = {
-    activeBackgroundColor: '#5637DD',
-    inactiveBackgroundColor: '#CEC8FF',
-    activeTintColor: '#fff',
-    inactiveTintColor: '#808080',
-    labelStyle: {fontSize: 16}
+    activeBackgroundColor: "#5637DD",
+    inactiveBackgroundColor: "#CEC8FF",
+    activeTintColor: "#fff",
+    inactiveTintColor: "#808080",
+    labelStyle: { fontSize: 16 },
   };
-  return(
-    <Tab.Navigator 
-    initialRouteName="Login" 
-    tabBarOptions={tabBarOptions}
-    screenOptions={{headerShown: false}}
+  return (
+    <Tab.Navigator
+      initialRouteName="Login"
+      tabBarOptions={tabBarOptions}
+      screenOptions={{ headerShown: false }}
     >
-      <Tab.Screen 
-        name='Login'
+      <Tab.Screen
+        name="Login"
         component={LoginTab}
         options={{
-          tabBarIcon: (props)=>{
-            return(
-              <Icon 
-                name='sign-in'
-                type='font-awesome'
-                color={props.color}
-              />
+          tabBarIcon: (props) => {
+            return (
+              <Icon name="sign-in" type="font-awesome" color={props.color} />
             );
-          }
+          },
         }}
       />
-      <Tab.Screen 
-        name='Register'
+      <Tab.Screen
+        name="Register"
         component={RegisterTab}
         options={{
-          tabBarIcon: (props)=>{
-            return(
-              <Icon 
-                name='user-plus'
-                type='font-awesome'
-                color={props.color}
-              />
-            )
-          }
+          tabBarIcon: (props) => {
+            return (
+              <Icon name="user-plus" type="font-awesome" color={props.color} />
+            );
+          },
         }}
       />
     </Tab.Navigator>
   );
-
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
-    margin: 20,
+    margin: 10,
   },
   formInput: {
-    padding: 10,
+    padding: 8,
+    height: 60
   },
   formIcon: {
     marginRight: 10,
   },
   formCheckBox: {
-    margin: 10,
-    backgroundColor: null
+    margin: 8,
+    backgroundColor: null,
   },
   formButton: {
-    margin: 40,
+    margin: 20,
+    marginRight: 40,
+    marginLeft: 40
   },
 });
 
